e73fc90510b5d77fad28d8d96acfd187d2eb8389
Steven Choi <stevencschoi@gmail.com>
2020-06-17 08:39:20 -0400

    initial commit

---
+++ experiments/episode-picker/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favourite episode!</p>
+    </>
+  );
+}
+
+export default App;
