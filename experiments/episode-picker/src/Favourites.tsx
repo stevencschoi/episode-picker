bd37a52ff3de74ae9fb7039bdb407098a99853af
Steven Choi <stevencschoi@gmail.com>
2020-06-17 11:44:55 -0400

    favourites displaying on separate page

--- experiments/episode-picker/src/Favourites.tsx
+++ experiments/episode-picker/src/Favourites.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { lazy, useContext } from "react";
+import { Store } from "./Store";
+import { toggleFavAction } from "./Actions";
 
-export default function Favourites() {
-  return <>Hello</>;
+const EpisodesList = lazy<any>(() => import("./EpisodesList"));
+
+export default function Favourites(): JSX.Element {
+  const { state, dispatch } = useContext(Store);
+
+  const props = {
+    episodes: state.favourites,
+    store: { state, dispatch },
+    toggleFavAction,
+    favourites: state.favourites,
+  };
+
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <div className="episode-layout">
+        <EpisodesList {...props} />
+      </div>
+    </React.Suspense>
+  );
 }
